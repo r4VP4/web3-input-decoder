ed4d249c594988ed7d095533ee5f7fdd17ee49b6
r4VP4 <github@wp1074399.server-he.de>
2022-12-09 15:32:37 +0100

    added web3_input_decoder.utils.get_selector_to_error_type

--- web3_input_decoder/utils.py
+++ web3_input_decoder/utils.py
@@ -31,6 +31,16 @@ def get_selector_to_function_type(abi: List[dict]) -> dict:
     return type_defs
 
 
+# works for solidity custom errors
+def get_selector_to_error_type(abi: List[dict]) -> dict:
+    type_defs = {}
+    for type_def in abi:
+        if type_def["type"] == "error":
+            selector = function_abi_to_4byte_selector(type_def)
+            type_defs[selector] = type_def
+    return type_defs
+
+
 def expand_tuple_types(type_def: dict) -> str:
     types = []
     for comp in type_def["components"]:
@@ -65,8 +75,8 @@ def hex_to_bytes(data: Union[str, bytes]) -> bytes:
 
 
 def detect_constructor_arguments(
-    type_def: dict,
-    tx_input_with_bytecode: Union[str, bytes],
+        type_def: dict,
+        tx_input_with_bytecode: Union[str, bytes],
 ) -> bytes:
     types, _ = get_types_names(type_def["inputs"])
     default_values: List[Any] = []
@@ -85,4 +95,4 @@ def detect_constructor_arguments(
             raise NotImplementedError(f"Type {t} is not implemented yet")
 
     default_args = encode(types, default_values)
-    return hex_to_bytes(tx_input_with_bytecode)[-len(default_args) :]
+    return hex_to_bytes(tx_input_with_bytecode)[-len(default_args):]
